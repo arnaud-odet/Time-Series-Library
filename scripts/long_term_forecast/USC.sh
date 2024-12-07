a2e689ea939cd93cb7a0e11ec0b77a6e8c2d36dc
Arnaud Odet <arnaud.odet.pro@gmail.com>
2024-12-07 14:49:35 +0100

    USC script

--- scripts/long_term_forecast/USC.sh
+++ scripts/long_term_forecast/USC.sh
@@ -10,8 +10,8 @@ data=USC
 features=M
 d_lstm=256
 lstm_layers=1
-e_layers=2
-d_model=512
+e_layers=1
+d_model=256
 n_heads=8
 d_layers=2
 d_fc=256 
@@ -23,8 +23,8 @@ des='Exp'
 batch_size=64
 itr=1
 dropout=0.25
-learning_rate=0.0000001
-optimizer=adam
+learning_rate=0.001
+optimizer=adamw
 wd=0.05
 train_epochs=10
 patience=3
@@ -73,6 +73,151 @@ python -u run.py \
   --data_path $data_path \
   --model_id USC_64_64 \
   --model PatchTST \
+  --data $data \
+  --input_features $input_features \
+  --use_action_progress $use_action_progress \
+  --use_offense $use_offense \
+  --features $features \
+  --seq_len 64 \
+  --label_len 12 \
+  --pred_len 64 \
+  --e_layers $e_layers \
+  --d_model $d_model \
+  --d_layers $d_layers \
+  --factor $factor \
+  --enc_in $enc_in \
+  --dec_in $dec_in \
+  --c_out $c_out \
+  --des $des \
+  --n_heads $n_heads \
+  --batch_size $batch_size \
+  --itr $itr \
+  --d_lstm $d_lstm \
+  --lstm_layers $lstm_layers \
+  --d_fc $d_fc \
+  --dropout $dropout \
+  --learning_rate $learning_rate \
+  --optimizer $optimizer \
+  --wd $wd \
+  --train_epochs $train_epochs \
+  --patience $patience 
+
+
+python -u run.py \
+  --task_name $task_name \
+  --is_training $is_training \
+  --root_path $root_path \
+  --data_path $data_path \
+  --model_id USC_64_64 \
+  --model Nonstationary_Transformer \
+  --data $data \
+  --input_features $input_features \
+  --use_action_progress $use_action_progress \
+  --use_offense $use_offense \
+  --features $features \
+  --seq_len 64 \
+  --label_len 12 \
+  --pred_len 64 \
+  --e_layers $e_layers \
+  --d_model $d_model \
+  --d_layers $d_layers \
+  --factor $factor \
+  --enc_in $enc_in \
+  --dec_in $dec_in \
+  --c_out $c_out \
+  --des $des \
+  --n_heads $n_heads \
+  --batch_size $batch_size \
+  --itr $itr \
+  --d_lstm $d_lstm \
+  --lstm_layers $lstm_layers \
+  --d_fc $d_fc \
+  --dropout $dropout \
+  --learning_rate $learning_rate \
+  --optimizer $optimizer \
+  --wd $wd \
+  --train_epochs $train_epochs \
+  --patience $patience 
+
+python -u run.py \
+  --task_name $task_name \
+  --is_training $is_training \
+  --root_path $root_path \
+  --data_path $data_path \
+  --model_id USC_64_64 \
+  --model TimesNet \
+  --data $data \
+  --input_features $input_features \
+  --use_action_progress $use_action_progress \
+  --use_offense $use_offense \
+  --features $features \
+  --seq_len 64 \
+  --label_len 12 \
+  --pred_len 64 \
+  --e_layers $e_layers \
+  --d_model $d_model \
+  --d_layers $d_layers \
+  --factor $factor \
+  --enc_in $enc_in \
+  --dec_in $dec_in \
+  --c_out $c_out \
+  --des $des \
+  --n_heads $n_heads \
+  --batch_size $batch_size \
+  --itr $itr \
+  --d_lstm $d_lstm \
+  --lstm_layers $lstm_layers \
+  --d_fc $d_fc \
+  --dropout $dropout \
+  --learning_rate $learning_rate \
+  --optimizer $optimizer \
+  --wd $wd \
+  --train_epochs $train_epochs \
+  --patience $patience 
+
+python -u run.py \
+  --task_name $task_name \
+  --is_training $is_training \
+  --root_path $root_path \
+  --data_path $data_path \
+  --model_id USC_64_64 \
+  --model iTransformer \
+  --data $data \
+  --input_features $input_features \
+  --use_action_progress $use_action_progress \
+  --use_offense $use_offense \
+  --features $features \
+  --seq_len 64 \
+  --label_len 12 \
+  --pred_len 64 \
+  --e_layers $e_layers \
+  --d_model $d_model \
+  --d_layers $d_layers \
+  --factor $factor \
+  --enc_in $enc_in \
+  --dec_in $dec_in \
+  --c_out $c_out \
+  --des $des \
+  --n_heads $n_heads \
+  --batch_size $batch_size \
+  --itr $itr \
+  --d_lstm $d_lstm \
+  --lstm_layers $lstm_layers \
+  --d_fc $d_fc \
+  --dropout $dropout \
+  --learning_rate $learning_rate \
+  --optimizer $optimizer \
+  --wd $wd \
+  --train_epochs $train_epochs \
+  --patience $patience 
+
+  python -u run.py \
+  --task_name $task_name \
+  --is_training $is_training \
+  --root_path $root_path \
+  --data_path $data_path \
+  --model_id USC_64_64 \
+  --model TimeMixer \
   --data $data \
   --input_features $input_features \
   --use_action_progress $use_action_progress \
