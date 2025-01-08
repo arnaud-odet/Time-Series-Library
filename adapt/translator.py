bca7defa629503a2e4900e20a8b1e664aaccf00b
Arnaud Odet <arnaud.odet.pro@gmail.com>
2025-01-08 15:42:25 +0100

    ADAPT : generating lane_data accurately

--- adapt/translator.py
+++ adapt/translator.py
@@ -23,6 +23,42 @@ def args_translator(args):
     
     return args
 
+def create_simple_lane_data(feature_size=128):
+    # Define your 4 lines coordinates
+    lines = [
+        [(0, 0), (0, 70)],  # Line 1
+        [(0, 70), (100, 70)],  # Line 2
+        [(100, 70), (100, 0)],  # Line 3
+        [(100, 0), (0, 0)]   # Line 4
+    ]
+    
+    lane_list = []
+    for line_idx, line in enumerate(lines):
+        vector = torch.zeros(feature_size)
+        
+        # Set current and previous positions
+        vector[-4:] = torch.tensor([
+            line[1][0],  # current x
+            line[1][1],  # current y
+            line[0][0],  # previous x
+            line[0][1]   # previous y
+        ])
+        
+        # Set pre-previous positions (you can adjust these)
+        vector[-17:-15] = torch.tensor([
+            line[0][0],  # pre-previous x
+            line[0][1]   # pre-previous y
+        ])
+        
+        # Set other relevant flags
+        vector[-5] = 1  # Binary flag
+        vector[-6] = line_idx  # Line index
+        vector[-7] = 0  # Polyline span index
+        
+        lane_list.append(vector)
+    
+    return lane_list
+
 def usc_data_translator(batch_x, args, verbose :bool = True):
     batch_size = batch_x.shape[0]
     offset = batch_x.shape[-1] - 30
@@ -36,7 +72,7 @@ def usc_data_translator(batch_x, args, verbose :bool = True):
         agent_data = [torch.cat((batch_x[i, :, p + offset : p + offset +2], pad),1)  for p in range(15)] 
 
         # Lane data
-        lane_data = [torch.zeros(1,args.hidden_size)]
+        lane_data = create_simple_lane_data(args.hidden_size)
         
         # Meta info
         d = [ ad[-1,:2] - ad[-2,:2] for ad in agent_data]
