1c01693afa8403bedcb3a3d06fdab907057b1c96
Arnaud Odet <arnaud.odet.pro@gmail.com>
2025-01-22 12:12:34 +0100

    New LSTransformer experiments

--- scripts/long_term_forecast/bball_LSTransformer.sh
+++ scripts/long_term_forecast/bball_LSTransformer.sh
@@ -1,4 +1,4 @@
-model=SABFormer
+model=LSTransformer
 task_name=long_term_forecast
 is_training=1
 root_path=./dataset/bball/
@@ -25,7 +25,6 @@ train_epochs=36
 patience=4
 
 
-
 python -u run.py \
   --model $model \
   --task_name $task_name \
@@ -39,10 +38,116 @@ python -u run.py \
   --features $features \
   --data $data \
   --lstm_layers 1 \
-  --d_lstm 256\
-  --e_layers 2 \
+  --d_lstm 256 \
+  --e_layers 3 \
   --d_model 256 \
-  --d_layers 2 \
+  --d_layers 1 \
+  --d_fc 256 \
+  --factor $factor \
+  --enc_in $enc_in \
+  --dec_in $dec_in \
+  --c_out $c_out \
+  --des $des \
+  --n_heads $n_heads \
+  --batch_size $batch_size \
+  --itr $itr \
+  --dropout $dropout \
+  --learning_rate $learning_rate \
+  --optimizer $optimizer \
+  --wd $wd \
+  --train_epochs $train_epochs \
+  --patience $patience \
+  --embed fixed \
+  --inverse
+
+  python -u run.py \
+  --model $model \
+  --task_name $task_name \
+  --is_training $is_training \
+  --root_path $root_path \
+  --data_path $data_path \
+  --model_id $model_id \
+  --seq_len $seq_len \
+  --label_len $label_len \
+  --pred_len $pred_len \
+  --features $features \
+  --data $data \
+  --lstm_layers 1 \
+  --d_lstm 256 \
+  --e_layers 4 \
+  --d_model 256 \
+  --d_layers 1 \
+  --d_fc 256 \
+  --factor $factor \
+  --enc_in $enc_in \
+  --dec_in $dec_in \
+  --c_out $c_out \
+  --des $des \
+  --n_heads $n_heads \
+  --batch_size $batch_size \
+  --itr $itr \
+  --dropout $dropout \
+  --learning_rate $learning_rate \
+  --optimizer $optimizer \
+  --wd $wd \
+  --train_epochs $train_epochs \
+  --patience $patience \
+  --embed fixed \
+  --inverse
+
+
+python -u run.py \
+  --model $model \
+  --task_name $task_name \
+  --is_training $is_training \
+  --root_path $root_path \
+  --data_path $data_path \
+  --model_id $model_id \
+  --seq_len $seq_len \
+  --label_len $label_len \
+  --pred_len $pred_len \
+  --features $features \
+  --data $data \
+  --lstm_layers 2 \
+  --d_lstm 256 \
+  --e_layers 3 \
+  --d_model 256 \
+  --d_layers 1 \
+  --d_fc 256 \
+  --factor $factor \
+  --enc_in $enc_in \
+  --dec_in $dec_in \
+  --c_out $c_out \
+  --des $des \
+  --n_heads $n_heads \
+  --batch_size $batch_size \
+  --itr $itr \
+  --dropout $dropout \
+  --learning_rate $learning_rate \
+  --optimizer $optimizer \
+  --wd $wd \
+  --train_epochs $train_epochs \
+  --patience $patience \
+  --embed fixed \
+  --inverse
+
+  python -u run.py \
+  --model $model \
+  --task_name $task_name \
+  --is_training $is_training \
+  --root_path $root_path \
+  --data_path $data_path \
+  --model_id $model_id \
+  --seq_len $seq_len \
+  --label_len $label_len \
+  --pred_len $pred_len \
+  --features $features \
+  --data $data \
+  --lstm_layers 2 \
+  --d_lstm 256 \
+  --e_layers 4 \
+  --d_model 256 \
+  --d_layers 1 \
   --d_fc 256 \
   --factor $factor \
   --enc_in $enc_in \
@@ -73,11 +178,46 @@ python -u run.py \
   --pred_len $pred_len \
   --features $features \
   --data $data \
-  --lstm_layers 2 \
-  --d_lstm 256\
-  --e_layers 2 \
+  --lstm_layers 3 \
+  --d_lstm 256 \
+  --e_layers 3 \
   --d_model 256 \
-  --d_layers 2 \
+  --d_layers 1 \
+  --d_fc 256 \
+  --factor $factor \
+  --enc_in $enc_in \
+  --dec_in $dec_in \
+  --c_out $c_out \
+  --des $des \
+  --n_heads $n_heads \
+  --batch_size $batch_size \
+  --itr $itr \
+  --dropout $dropout \
+  --learning_rate $learning_rate \
+  --optimizer $optimizer \
+  --wd $wd \
+  --train_epochs $train_epochs \
+  --patience $patience \
+  --embed fixed \
+  --inverse
+
+  python -u run.py \
+  --model $model \
+  --task_name $task_name \
+  --is_training $is_training \
+  --root_path $root_path \
+  --data_path $data_path \
+  --model_id $model_id \
+  --seq_len $seq_len \
+  --label_len $label_len \
+  --pred_len $pred_len \
+  --features $features \
+  --data $data \
+  --lstm_layers 3 \
+  --d_lstm 256 \
+  --e_layers 4 \
+  --d_model 256 \
+  --d_layers 1 \
   --d_fc 256 \
   --factor $factor \
   --enc_in $enc_in \
