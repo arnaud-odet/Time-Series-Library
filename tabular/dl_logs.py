3e713ea2c6a5066d516576dbbd22acf2fa1f648f
Arnaud Odet <arnaud.odet.pro@gmail.com>
2025-05-04 12:12:58 +0200

    ResultReader class

--- tabular/dl_logs.py
+++ tabular/dl_logs.py
@@ -17,6 +17,113 @@ CALMIP_START_INDEX = {
 
 # Utils
 
+class ResultReader:
+    
+    def __init__(self, 
+                 time_horizons:list,
+                 runs : list,
+                 metric : str = 'fde'):
+        self.df = pd.read_csv(os.path.join(LOG_PATH,'calmip_logs.csv'), index_col = 0)
+        self.runs = runs
+        self.metric = metric
+        self.ths = time_horizons
+        self._assign_logs_to_run()
+        self.score_baselines()
+        self.read_logs()
+            
+    def _assign_logs_to_run(self):
+        self.df['run'] = 0
+        for index, row in self.df.iterrows():
+            if index < CALMIP_START_INDEX[2]:
+                self.df.loc[index,'run'] = 1
+            elif index < CALMIP_START_INDEX[3]:
+                self.df.loc[index,'run'] = 2
+            else :
+                if row['features'] == 'MS' and row['dec_in'] == 1 :
+                    self.df.loc[index,'run'] = 3
+                else :
+                    self.df.loc[index,'run'] = 4        
+    
+    @staticmethod
+    def create_model_identifier(row):
+        return f"run{row['run']}_{row['model']}_{row['features']}_{row['seq_len']}-{row['pred_len']}_el{row['e_layers']}_dm{row['d_model']}_nh{row['n_heads']}_dl{row['d_layers']}_ff{row['d_ff']}_do{row['dropout']}"
+
+    @staticmethod
+    def create_model_architecture(row):
+        return f"el{row['e_layers']}_dm{row['d_model']}_nh{row['n_heads']}_dl{row['d_layers']}_ff{row['d_ff']}"
+
+    def _add_model_identifier(self):
+        self.df['model_id'] = self.df.apply(lambda row : self.create_model_identifier(row), axis =1)
+        self.df['arch'] = self.df.apply(lambda row : self.create_model_architecture(row), axis =1)
+        
+    # Baselines
+    @staticmethod
+    def score_baseline(args, seq_len : int, pred_len : int):
+        args.seq_len = seq_len
+        args.pred_len = pred_len
+        args.root_path = DATA_PATH
+        args.scale = False
+        uscds, uscdl = data_provider(args= args, flag = 'test', )
+        ### StandStill
+        ss_fde = np.sqrt(np.mean(uscds.data_y[:,-1,0]**2))
+        ss_rmse = np.sqrt(np.mean(uscds.data_y[:,:,0]**2))
+        ### Constant Velocity
+        bl_cv_delta = uscds.data_x[:,-1,0] - uscds.data_x[:,-2,0]
+        bl_cv_pred = np.repeat(uscds.data_x[:,-1,0].reshape(-1,1,1), uscds.data_y.shape[1], axis=1)
+        for i in range(uscds.data_y.shape[1]):
+            bl_cv_pred[:,i,0] = (i+1) * bl_cv_delta 
+        cv_fde = np.sqrt(np.mean( (uscds.data_y[:,-1,0] - bl_cv_pred[:,-1,0])**2 ))
+        cv_rmse = np.sqrt(np.mean( (uscds.data_y[:,:,0] - bl_cv_pred[:,:,0])**2 ))
+        ### Concat
+        bls = [{'model':'Baseline - StandStill', 'features':'S', 'time':f"{seq_len}-{pred_len}",'fde':ss_fde, 'rmse':ss_rmse},
+            {'model':'Baseline - ConstantVelocity', 'features':'S', 'time':f"{seq_len}-{pred_len}",'fde':cv_fde, 'rmse': cv_rmse}]
+        
+        return bls
+      
+    def score_baselines(self):
+        args = SimpleNamespace(**self.df.loc[0].to_dict())
+        bls = []
+        for th in self.ths :
+            bls += self.score_baseline(args, th,th)
+        self.bls = pd.DataFrame(bls)
+        self.bls = self.bls[['model', 'features', 'time', self.metric]].rename(columns = {self.metric : 'mean'})
+        self.bls['std'] = 0
+        self.bls['run'] = 'n.a.'
+        self.bls['arch'] = 'n.a.'
+        self.bls = self.bls.pivot(index = ['model','features'], columns = 'time', values= ['mean', 'std']).swaplevel(0,1,axis = 1).sort_index(axis = 1)
+        
+    # Log processing and concatenation with baselines
+    def read_logs(self):
+        run_query = (self.df['run'].isin(self.runs))
+        self.df = self.df[run_query]
+        
+        # Processing logs
+        self._add_model_identifier()
+        self.df['time'] = self.df.apply(lambda row : f"{row['seq_len']}-{row['pred_len']}", axis =1)
+        temp_df = self.df[['model_id','run','arch','model','features','time',self.metric]].groupby('model_id').agg(
+            model=pd.NamedAgg(column='model', aggfunc='first'),
+            run=pd.NamedAgg(column='run', aggfunc='first'),
+            arch=pd.NamedAgg(column='arch', aggfunc='first'),
+            features=pd.NamedAgg(column='features', aggfunc='first'),
+            time=pd.NamedAgg(column='time', aggfunc='first'),
+            count=pd.NamedAgg(column= self.metric, aggfunc='count'),
+            mean=pd.NamedAgg(column= self.metric, aggfunc='mean'),
+            std=pd.NamedAgg(column= self.metric, aggfunc='std'),
+        ).sort_values(by = 'mean').reset_index()
+        temp_df['dupl'] = temp_df[['model','time','features']].duplicated()
+        temp_df['std'] = temp_df['std'].fillna(0)
+        summary_df = temp_df[temp_df['dupl'] == False].copy()
+        self.results = summary_df.copy()
+        summary_df = summary_df.pivot(index = ['model','features'], columns = 'time', values = ['mean','std']).swaplevel(0,1,axis = 1).sort_index(axis = 1)
+        summary_df = pd.concat([summary_df,self.bls], axis = 0)            
+        summary_df[('Average','mean')] = summary_df.xs('mean', axis = 1, level = 1).mean(axis = 1)
+        self.results_df = summary_df.sort_values(by = ('Average','mean'))
+           
+
+
+
+# OLD
+
 def create_model_identifier(row):
     return f"{row['model']}_{row['features']}_{row['seq_len']}-{row['pred_len']}_el{row['e_layers']}_dm{row['d_model']}_nh{row['n_heads']}_dl{row['d_layers']}_ff{row['d_ff']}_do{row['dropout']}"
 
